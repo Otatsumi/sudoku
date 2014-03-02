732f46d28637fe052348ea02f07e7fbb6bc529f2
broggi_t <broggi_t@epitech.eu>
2014-03-02 19:47:05 +0100

    free ;)

--- srcs/main.c
+++ srcs/main.c
@@ -5,21 +5,33 @@
 ** Login   <bauwen_j@epitech.net>
 ** 
 ** Started on  Sun Mar  2 19:16:39 2014 bauwens
-** Last update Sun Mar  2 19:22:08 2014 bauwens
+** Last update Sun Mar  2 19:46:45 2014 
 */
 
 #include <stdlib.h>
 #include "my_str.h"
 #include "sudo.h"
 
+void		free_grid(char **grid)
+{
+  int		i;
+
+  i = -1;
+  while (++i < 9)
+    free(grid[i]);
+  free(grid);
+}
+
 int		main(void)
 {
-  char		**grid;;
+  char		**grid;
+
   while ((grid = get_grid()) != NULL)
     {
       if (algo(grid) == FAILURE)
 	my_puterr(INVALID_GRID);
       display(grid);
+      free_grid(grid);
     }
   return (0);
 }
