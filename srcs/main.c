2b73da01e4aee7cd6bc1f0711b06b37b0ebfd6c0
bauwen_j <bauwen_j@epitech.eu>
2014-03-02 20:13:25 +0100

    add #

--- srcs/main.c
+++ srcs/main.c
@@ -5,7 +5,7 @@
 ** Login   <bauwen_j@epitech.net>
 ** 
 ** Started on  Sun Mar  2 19:16:39 2014 bauwens
-** Last update Sun Mar  2 19:46:45 2014 
+** Last update Sun Mar  2 20:12:27 2014 bauwens
 */
 
 #include <stdlib.h>
@@ -25,13 +25,21 @@ void		free_grid(char **grid)
 int		main(void)
 {
   char		**grid;
+  int		i;
 
+  i = 0;
   while ((grid = get_grid()) != NULL)
     {
+      if (i != 0)
+	{
+	  my_putnchar('#', 20);
+	  my_putchar('\n');
+	}
       if (algo(grid) == FAILURE)
 	my_puterr(INVALID_GRID);
       display(grid);
       free_grid(grid);
+      i++;
     }
   return (0);
 }
